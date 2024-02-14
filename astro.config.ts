be12e9debaae3cab10f539599266708968a6afca
acane <aseuo0726@gmail.com>
2024-02-14 18:58:27 +0800

    first commit

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-
-import UnoCSS from 'unocss/astro'
+import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [UnoCSS({
-    injectReset: true,
+    injectReset: true
   })]
 });
